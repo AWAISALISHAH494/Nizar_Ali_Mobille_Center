bd0b62b60e4db3543237d8935e8c786695698377
awais khan <awaisalishah02@gmail.com>
2025-11-25 00:33:06 +0500

    Fix static files path

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -523,7 +523,7 @@ if USE_S3:
 else:
     STATIC_URL = "/static/"
     STATIC_ROOT = BASE_DIR / "staticfiles"
-    STATICFILES_DIRS = [BASE_DIR / "static"]
+    STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
     MEDIA_URL = "/media/"
     MEDIA_ROOT = BASE_DIR / "media"
